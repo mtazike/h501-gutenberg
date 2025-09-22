3097758a9f7b8b8d0ac599c8b63b7d5b326d7e60
Mahya <mtazike@iu.edu>
2025-09-22 15:04:07 -0400

    Exercise 3: tt_gutenberg package

--- Setup.py
+++ Setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="tt_gutenberg",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Mahya Tazike",
+    author_email="mtazike@iu.edu",
     description="A package for working with Project Gutenberg author data",
     packages=find_packages(),
     install_requires=[
